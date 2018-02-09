3c9fa241718b522bbece6e9205e9bbb2c69995a9
Andre Weissflog <floooh@gmail.com>
2018-02-09 18:58:00 +0100

    Initial commit

---
+++ clear.nim
@@ -0,0 +1,40 @@
+import glfw3 as glfw
+import opengl
+import sokol/gfx as sg
+import sokol/flextgl as flextgl
+
+# initialize GLFW, FlextGL and sokol
+if glfw.Init() != 1:
+    quit(QUIT_FAILURE)
+glfw.WindowHint(CONTEXT_VERSION_MAJOR, 3)
+glfw.WindowHint(CONTEXT_VERSION_MINOR, 3)
+glfw.WindowHint(OPENGL_PROFILE, OPENGL_CORE_PROFILE)
+glfw.WindowHint(OPENGL_FORWARD_COMPAT, GL_TRUE.cint)
+let win = glfw.CreateWindow(640, 480, "Triangle (sokol-nim)", nil, nil)
+glfw.MakeContextCurrent(win)
+flextgl.init()
+var desc = sg.sg_desc()
+sg.setup(desc)
+
+# a pass action (clear to color)
+var pass_action = sg.sg_pass_action()
+pass_action.colors[0].action = SG_ACTION_CLEAR
+pass_action.colors[0].val = [1.0f, 0.0f, 0.0f, 1.0f]
+
+# draw loop
+while glfw.WindowShouldClose(win) == 0:
+    # animate clear color
+    var g = pass_action.colors[0].val[1] + 0.01
+    if (g > 1.0f): g = 0.0f
+    pass_action.colors[0].val[1] = g;
+
+    var w, h: cint
+    glfw.GetFramebufferSize(win, addr(w), addr(h))
+    sg.begin_default_pass(pass_action, w, h)
+    sg.end_pass()
+    sg.commit()
+    glfw.PollEvents()
+    glfw.SwapBuffers(win)
+
+sg.shutdown()
+glfw.Terminate()
---
+++ sokol_nim_samples.nimble
@@ -0,0 +1,16 @@
+# Package
+
+version       = "0.1.0"
+author        = "Andre Weissflog"
+description   = "Example code for https://github.com/floooh/sokol-nim"
+license       = "MIT"
+
+bin = @["clear"]
+
+# Dependencies
+
+requires "nim >= 0.17.2"
+requires "https://github.com/floooh/sokol-nim.git"
+requires "nimrod-glfw >= 3.2.0"
+requires "opengl >= 1.1"
+
